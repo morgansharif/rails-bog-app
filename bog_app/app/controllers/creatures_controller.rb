92ffb6ff1e267b8c55a023f6c1fae139aac5c9ae
morgansharif <morgan.sharif@gmail.com>
2016-05-01 23:16:23 -0700

    update

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -6,15 +6,27 @@ class CreaturesController < ApplicationController
   def new
     @creature = Creature.new
   end
+
   def create
     creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
-      redirect_to creatures_path
+      redirect_to creature_path(creature)
     end
   end
+
   def show
     @creature = Creature.find(params[:id])
   end
 
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    creature = Creature.find(params[:id])
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
 end
