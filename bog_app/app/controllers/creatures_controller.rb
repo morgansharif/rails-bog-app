8e7b0c7d8917c09ad0767eeab0fadf96440582c7
morgansharif <morgan.sharif@gmail.com>
2016-05-01 17:19:28 -0700

    update

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -9,14 +9,11 @@ class CreaturesController < ApplicationController
     render :new
   end
 
-  # create a new creature in the database
   def create
     # whitelist params and save them to a variable
     creature_params = params.require(:creature).permit(:name, :description)
-
     # create a new creature from `creature_params`
     creature = Creature.new(creature_params)
-
     # if creature saves, redirect to route that displays all creatures
     if creature.save
       redirect_to creature_path(creature)
@@ -29,6 +26,19 @@ class CreaturesController < ApplicationController
     creature_id = params[:id]
     @creature = Creature.find(creature_id)
     render :show
+  end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find(creature_id)
+    render :edit
+  end
+
+  def update
+    @creature = Creature.find(params[:id])
+    creature_params = params.require(:creature).permit(:name, :description)
+    @creature.update(creature_params)
+    redirect_to creature_path(@creature)
   end
 
 end
