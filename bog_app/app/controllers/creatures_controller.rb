f2d2b3db7acc160fe9c6463ab7492e6fa43bffe0
morgansharif <morgan.sharif@gmail.com>
2016-05-01 20:24:14 -0700

    new

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -2,7 +2,21 @@ class CreaturesController < ApplicationController
 
   def index
     @creatures = Creature.all
-    render :index
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creatures_path()
+    end
+  end
+
+  def show
   end
 
 end
